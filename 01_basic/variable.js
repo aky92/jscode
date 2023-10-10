5aa8f26ef833699394bba5f7141176c4e771f0fc
aky.tari92 <aky.tari92@gmail.com>
2023-10-10 15:01:04 +0000

    dataconversion in Number, Boolean , String

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -6,7 +6,7 @@
 // let credit_score=null;
 // let credit_score=undefined;
 // let credit_score=true;
-let credit_score="akshay";
+let credit_score=225;
 
 
 console.log(credit_score);
@@ -14,7 +14,30 @@ console.log(typeof(credit_score));
 
 //now we are trying to convert variable credit_score into Number only
 
-let convo=Number(credit_score);
-console.log(typeof(convo));
-console.log(convo);
+// let convo=Number(credit_score);
+// console.log(typeof(convo));
+// console.log(convo);
+// console.log(credit_score);
+
+let convo2=String(credit_score);
+console.log(convo2);
 console.log(credit_score);
+console.log(typeof(convo2));
+
+/*results
+for Number
+123 >> 123 - number
+"123" >> 123
+"abc" >> NAN
+
+null >> null
+
+undifined >> undifined
+
+forBoolean
+1 >> true
+0 >> false
+"" >> false
+"akshay" >> true
+
+*/
