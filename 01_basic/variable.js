2b5d4e22387a1c203b347b359c9dbb833b627e2e
aky.tari92 <aky.tari92@gmail.com>
2023-10-10 14:30:03 +0000

    vdatatype conversion

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,3 +1,18 @@
-let a=12.5;
 
-console.log(typeof(a));
+// let credit_score="599";
+// let credit_score=599;
+// let credit_score=null;
+// let credit_score=undefined;
+// let credit_score=true;
+let credit_score="akshay";
+
+
+console.log(credit_score);
+console.log(typeof(credit_score));
+
+//now we are trying to convert variable credit_score into Number only
+
+let convo=Number(credit_score);
+console.log(typeof(convo));
+console.log(convo);
+console.log(credit_score);
