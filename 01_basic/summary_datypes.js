7dca74cce51d9e87dc32b80f254faf53ec7c74ca
aky.tari92 <aky.tari92@gmail.com>
2023-10-12 02:21:56 +0000

    datatupe summary

---
+++ 01_basic/summary_datypes.js
@@ -0,0 +1,33 @@
+/*datatypes is diveded in to 2 parts 
+premitive and nonpremitive based on how to store in  memory  ans access   
+
+premitive(value type)
+-these datatypes are call by value type that means when you use this types systen
+wont give original insted it gives copy and changes done only in copy 
+
+string,number,boolean,null,undefine(means you declare variable but not define it),Symbol,BigInt
+
+
+
+
+nonpremitive (referance type)
+allowcate direct referance to you
+Array
+Objects
+function
+
+Q] javascript is static type language or dynamic type language
+--- dynamic type languge
+  you not need to spacify variable type while declearing it, js engin check at runtime
+
+*/
+
+
+// const Id=Symbol('123');
+// const p_Id=Symbol('123');
+// console.log(Id===p_Id);
+
+
+// let accout_bal=874546465712152542123231321321n; //used n at last to use bigint
+
+// console.log(typeof(accout_bal))    
