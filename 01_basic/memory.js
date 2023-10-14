030b92ac22c7d20c051beffe01b5876996616a5d
aky.tari92 <aky.tari92@gmail.com>
2023-10-14 03:12:50 +0000

    memory in js

---
+++ 01_basic/memory.js
@@ -0,0 +1,10 @@
+/*
+  there are two types of momory
+  1.stack - used in primitive (value type)
+  2.heap - used in non-premitive (referance type)
+
+  in stack original value is store as it is its copy made if that variable stored in another variable
+
+  but in heap memory original value is referance as new value so if new value changed it is affected on onriginal value
+
+  */
