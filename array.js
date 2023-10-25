50dfc262f85e6742386a0c15f2b937541a1435c4
aky.tari92 <aky.tari92@gmail.com>
2023-10-25 17:55:48 +0000

    array splice and slice

--- array.js
+++ array.js
@@ -4,10 +4,38 @@
 let fruits=["apple","pineapple","mango"];
 console.log(fruits);
 
-fruits.push("banana");
+fruits.push(100);
 console.log(fruits);
 
 
-let x=fruits.pop();
+// let x=fruits.pop();
 console.log(fruits);
-console.log(x);
+// console.log(x);
+
+
+//some array methods
+//includes-it find element in array
+// console.log(fruits.includes('100'));
+
+//indexof
+// console.log(fruits.indexOf(100));
+
+//join-it join whole elents in string
+
+// console.log(typeof fruits.join());
+
+
+//slice-it just splice element of array butnot affect og array and 
+//slice(index,finalvalue)it show index value and final value -1
+let fr1=fruits.slice(1,3);
+console.log(fr1);
+console.log(fruits);
+
+
+
+//splice-it just splice element of array and affect og array and 
+//splice(index,finalvalue)it show index value and final value 
+let fr2=fruits.splice(1,3);
+console.log(fr2);
+console.log(fruits);
+
