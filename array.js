9d84349685b90ec3c42ae4329603c16b5c049fc3
aky.tari92 <aky.tari92@gmail.com>
2023-10-25 15:46:53 +0000

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+// array is a collection of elment (with same type or mixed) in single variable
+//javascript elements are resizeable
+//javascript array copy opration create shalow copies
+let fruits=["apple","pineapple","mango"];
+console.log(fruits);
+
+fruits.push("banana");
+console.log(fruits);
+
+
+let x=fruits.pop();
+console.log(fruits);
+console.log(x);
