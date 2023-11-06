5d28dc437b74f6a575764ccc6f20537bbe83cc97
aky.tari92 <aky.tari92@gmail.com>
2023-11-06 17:50:55 +0000

    function decleration snd expression

--- clouser.js
+++ clouser.js
@@ -1,15 +1,29 @@
-function one(){
-    let a=10
-    function two(){
-        let b=20
-         console.log(a)
-        console.log(b)
+// function one(){
+//     let a=10
+//     function two(){
+//         let b=20
+//          console.log(a)
+//         console.log(b)
 
-    }
-    two()
+//     }
+//     two()
     // console.log(b)
-    console.log("test")
+    // console.log("test")
     
-}
+// }
 //one()
+//++++++++++++important aspect of sequnce of declearing function and defing function
 
+//Type 1 --function decleration
+drive1();
+function drive1(){
+    console.log("driver");
+}
+
+//type2--function expression--store function in variable 
+//it can not access before initilization
+drive2();
+let drive2=function(){
+    console.log("driver2");
+}
+// drive2();
