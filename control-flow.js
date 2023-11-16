2aad53db40686c7748a7023186248d825600bb52
aky.tari92 <aky.tari92@gmail.com>
2023-11-16 18:02:03 +0000

    control flow2

--- control-flow.js
+++ control-flow.js
@@ -16,9 +16,93 @@ otherwise if any one wants to register only register code work
 //comparrison opretor
 // =,!=,<(lessthyan),>(greter than),<=,>=,==,===
 
-let temp=35
-if(temp==35){console.log("normal temp")}
-else if(temp<35){console.log("below normal temp")}
-else if(temp>35){console.log("above normal temp")}
-else if(temp!=35){console.log("error")}
+// let temp=35
+// if(temp==35){console.log("normal temp")}
+// else if(temp<35){console.log("below normal temp")}
+// else if(temp>35){console.log("above normal temp")}
+// else if(temp!=35){console.log("error")}
 
+
+//switch case
+// let balance=500
+
+// switch(balance){
+//         case 300:
+//         console.log("a");
+//         break;
+
+//         case 550:
+//             console.log("b");
+//             break;
+
+//         default:
+//             console.log("not foumd")
+//             break;
+        
+
+
+// }
+/*
+truthy value and falsey value
+when we assume the value is true or false
+
+*/
+//exmaple
+let userName="abc.kk.com"
+
+//here we assume value is true
+if(userName){
+    console.log("logged in")
+}else{
+    console.log("not logged in")
+}
+
+
+let userName2=""
+
+//here we assume value is false--empty sring is falsy value
+if(userName2){
+    console.log("logged in2")
+}else{
+    console.log("not logged in2")
+}
+
+//below is list of all and only flasey vlaues
+// "",0,-0,null,undefined,NaN,Bigint 0n,false
+
+//below is list of all and only truthy vlaues
+//"0","false"," ",[],{},function(){} --all values in sring format
+
+
+//how to detect wether array is empty or not
+let sal=['dd'];
+if(sal.length==0){console.log("empty")}else{console.log("not empty")}
+
+
+//how to detect wether object is empty or not
+
+let tt={}
+if(Object.keys(tt).length===0){console.log("empty2")}else{console.log("not empty2")}
+
+
+//-----------------------------------------------------------------------------------------
+//nullish coloesense opretor--null ?? undifined
+// let qw;
+// qw=5 ?? 15;
+// console.log(qw)
+
+
+// let qw;
+// qw=null ?? 15;
+// console.log(qw)
+
+
+let qw;
+qw=undefined ?? 15;
+console.log(qw)
+
+//------------------------------------------------------------
+
+
+//terenery opreator
+//condition?true:false
