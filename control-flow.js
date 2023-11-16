16b55df7ff9262ae631351c64ee57f8fe39def3b
aky.tari92 <aky.tari92@gmail.com>
2023-11-16 14:27:57 +0000

    controkl-flow

---
+++ control-flow.js
@@ -0,0 +1,24 @@
+/*
+control flow:
+when you run code in real time it not always works all code , it 
+run only coditional code snippet 
+ex.if user logged in only logged in code logic work 
+otherwise if any one wants to register only register code work
+*/
+
+//example-forlogin code
+// let userLoggedin=false;
+// let userLoggedin=true;
+// if(userLoggedin){console.log("loggedin successfully")}
+
+// console.log("welcome to dmart")
+
+//comparrison opretor
+// =,!=,<(lessthyan),>(greter than),<=,>=,==,===
+
+let temp=35
+if(temp==35){console.log("normal temp")}
+else if(temp<35){console.log("below normal temp")}
+else if(temp>35){console.log("above normal temp")}
+else if(temp!=35){console.log("error")}
+
