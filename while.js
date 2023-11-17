13d965169a1aa28a0be80f9f323f17191bbfdff0
aky.tari92 <aky.tari92@gmail.com>
2023-11-17 17:27:40 +0000

    while-do-while

---
+++ while.js
@@ -0,0 +1,25 @@
+
+// let i=0;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+
+//array in while loop
+let fruits=['banana','apple','mango'];
+let i=0;
+while(i<=fruits.length-1){
+    console.log(fruits[i]);
+    i++
+}
+
+//do-while loop
+//the difference between while and do while is in do while statement execute 1st then fullfill conditions
+//and if condition is not stisfy , the staement print at once only
+let p=1;
+
+do{
+    console.log(p);
+    p++
+}while(p<=10);
