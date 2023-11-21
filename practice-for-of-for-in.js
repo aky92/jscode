74fdf5d0f2831e30fd77987a403348a429095143
aky.tari92 <aky.tari92@gmail.com>
2023-11-21 15:13:31 +0000

    practice -for-of and for-in oop

---
+++ practice-for-of-for-in.js
@@ -0,0 +1,48 @@
+
+
+//for-of //itrates values [used for array,map]
+
+//for-of on array
+// let school=["abc","PQR","xyz"]
+
+// for(i of school){
+//  let a=0;
+//     if(a==20){
+//     console.log(i);
+//    }
+// }
+
+
+
+// let colg=new Map();
+// colg.set("year",2023)
+// colg.set("year1",2024)
+// colg.set("year2",2025)
+// console.log(colg);
+
+
+// for([i,j] of colg){
+//     console.log(i);
+
+// }
+
+
+//for-in loop is basically used to itrate keys (mostly used in objets)
+
+// let pg={"name":"akshay","room_no":521}
+
+// for(i in pg){
+//     console.log(pg[i]);
+// }
+
+
+let frt=new Map();
+frt.set("apple","vit-D")
+frt.set("banana","vit-a")
+frt.set("jkfruit","vit-z")
+
+//console.log(frt);
+
+for(i in frt){
+    console.log(i);
+}
